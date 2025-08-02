a20861971ad5805a6e9fedd663d7ab71281eeb77
patapata5500 <puyochan_uo@hotmail.com>
2025-08-02 20:10:06 +0900

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
+  server: {
+    open: true,
+  },
+});
